b72347d4b5f098e4809ccfef74c8d068e3e342e6
numpynewb33 <benlegrand@icloud.com>
2022-03-02 13:29:56 -0600

    testmeta

--- src/App.svelte
+++ src/App.svelte
@@ -21,7 +21,7 @@
   const HEADER_TITLE = "MetaOps";
   const HEADER_LINK = "https://metaopsgaming.com/";
   // Your image or GIF needs to be in the /public folder for this to work
-  const IMAGE_LINK = "/metaops.png";
+  const IMAGE_LINK = "/metaops.jpg";
   /***********************************/
 
   let { solana } = window as any;
